6d55dd1a630728dd35f961d1c0e86481360732b9
alyssa-sharp <alykatherineee@gmail.com>
2022-04-29 12:37:43 -0400

    test c includes tests for approx, use as template

--- exact_solution/exact_tests.sh
+++ exact_solution/exact_tests.sh
@@ -6,13 +6,15 @@ echo TEST A - expected: 3
 echo TEST B - output: $(time python3 cs412_np_exact.py < test_cases/test_b.txt)
 echo TEST B - expected: 3
 
-echo TEST C - output: $(time python3 cs412_np_exact.py < test_cases/test_c.txt)
-echo TEST C - expected: 3
+echo ----------------------------
+
+echo TEST C EXACT - output: $(time python3 cs412_np_exact.py < test_cases/test_c.txt)
+echo TEST C EXACT - expected: 3
 
 #takes around 30 seconds
-echo TEST C - output: $(time python3 cs412_np_exact.py < test_cases/test_d.txt)
-echo TEST C - expected: 8
+echo TEST C APPROX - output: $(time python3 ../approximation_solution/cs412_mingraph_approximation_a.py < test_cases/test_d.txt)
+echo TEST C APPROX - expected: 8
 
-# 15 minutes??
+# 12 minutes
 # echo TEST 9 - output: $(time python3 cs412_np_exact.py < test_9.txt)
 # echo TEST 9 - expected: 3
