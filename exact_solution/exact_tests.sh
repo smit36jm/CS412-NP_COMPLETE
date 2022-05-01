8d8560bdfedaff29dd4d587b008237bb25f7b994
alyssa-sharp <alykatherineee@gmail.com>
2022-05-01 16:48:51 -0400

    updated with approximation tests

--- exact_solution/exact_tests.sh
+++ exact_solution/exact_tests.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-#echo TEST A - expected: 3
-echo TEST A - output: $(time python3 cs412_np_exact.py < test_cases/test_a.txt)
-echo TEST A - expected: 3
+echo TEST A EXACT - output: $(time python3 cs412_np_exact.py < test_cases/test_a.txt)
+echo TEST A EXACT - expected: 3
 
-echo TEST B - output: $(time python3 cs412_np_exact.py < test_cases/test_b.txt)
-echo TEST B - expected: 3
+echo TEST A APPROX - output: $(time python3 ../approximation_solution/cs412_mingraph_approximation_a.py < test_cases/test_a.txt)
+echo TEST A APPROX - expected: 3
+
+echo ----------------------------
+
+echo TEST B EXACT - output: $(time python3 cs412_np_exact.py < test_cases/test_b.txt)
+echo TEST B EXACT - expected: 3
+
+echo TEST B APPROX - output: $(time python3 ../approximation_solution/cs412_mingraph_approximation_a.py < test_cases/test_b.txt)
+echo TEST B APPROX - expected: 3
 
 echo ----------------------------
 
@@ -12,9 +19,9 @@ echo TEST C EXACT - output: $(time python3 cs412_np_exact.py < test_cases/test_c
 echo TEST C EXACT - expected: 3
 
 #takes around 30 seconds
-echo TEST C APPROX - output: $(time python3 ../approximation_solution/cs412_mingraph_approximation_a.py < test_cases/test_d.txt)
-echo TEST C APPROX - expected: 8
+echo TEST C APPROX - output: $(time python3 ../approximation_solution/cs412_mingraph_approximation_a.py < test_cases/test_c.txt)
+echo TEST C APPROX - expected: 3
 
-# 12 minutes
+# 15 minutes??
 # echo TEST 9 - output: $(time python3 cs412_np_exact.py < test_9.txt)
 # echo TEST 9 - expected: 3
